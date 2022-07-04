1f43a6c8edcdc138a94c8e644dec43123e9ef8bd
DimitrisMoraitis <dimitris.moraitis75@gmail.com>
2022-07-04 18:32:07 -0400

    created arithmetic exercises from slides

---
+++ class_2/simple_arithmetic.py
@@ -0,0 +1,48 @@
+# integer arithmetic
+x = 14
+y = 8
+
+result1 = x + y
+result2 = x - y
+result3 = x * y
+result4 = x / y
+
+print("Exercise1")
+print(result1)
+print(result2)
+print(result3)
+print(result4)
+
+# double arithmetic
+a = 8.5
+b = 3.4
+
+result5 = a + b
+result6 = a - b
+result7 = a * b
+result8 = a / b
+
+print("Exercise2")
+print(result5)
+print(result6)
+print(result7)
+print(result8)
+
+# arithmetic operations
+d = 9
+f = 4
+
+add = d + f
+sub = d - f
+mul = d * f
+div1 = d / f
+div2 = d // f
+mod = d % f
+
+print("Exercise3")
+print(add)
+print(sub)
+print(mul)
+print(div1)
+print(div2)
+print(mod)
