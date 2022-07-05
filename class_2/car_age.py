5142ba2e2ef2a31e50ecb7e27507991b95897844
DimitrisMoraitis <dimitris.moraitis75@gmail.com>
2022-07-05 18:31:51 -0400

    added determination of even/odd using == operator

--- class_2/car_age.py
+++ class_2/car_age.py
@@ -1,11 +1,14 @@
-caryear = input("Enter the year of your car > ")
-currentyear = input("Enter current year > ")
+car_year = input("Enter the purchase year of your car > ")
+current_year = input("Enter current year > ")
 
-curryear = (int(currentyear)-int(caryear))
-currmonth = (int(currentyear)-int(caryear)) * 12
-currdays = (int(currentyear)-int(caryear)) * 365
+car_year = int(car_year)
+current_year = int(current_year)
+
+year = (current_year-car_year)
+month = (current_year-car_year) * 12
+days = (current_year-car_year) * 365
 
 
-print("Your car is " + str(curryear) + " years old")
-print("Your car is " + str(currmonth) + " months old")
-print("Your car is " + str(currdays) + " days old")
+print("Your car is " + str(year) + " years old")
+print("Your car is " + str(month) + " months old")
+print("Your car is " + str(days) + " days old")
