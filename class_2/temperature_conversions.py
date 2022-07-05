6c46f55f6e6fda6c42001286b506bae8679eb570
DimitrisMoraitis <dimitris.moraitis75@gmail.com>
2022-07-05 18:48:56 -0400

    spacings in equation

--- class_2/temperature_conversions.py
+++ class_2/temperature_conversions.py
@@ -3,7 +3,7 @@
 temp = input("Enter temperature to convert > ")
 temp = int(temp)
 
-farenheit = round((9/5)*temp + 32, 1)
+farenheit = round((9/5) * temp + 32, 1)
 celsius = round((temp - 32) / (9/5), 1)
 
 print("The temperature in Fareneheit is " + str(farenheit) + " degrees F")
