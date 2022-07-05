62ff67ecd67977dab83d17c8550b3efe15350b89
DimitrisMoraitis <dimitris.moraitis75@gmail.com>
2022-07-05 18:47:45 -0400

    adding spacing between exercises by using triple quotes

--- class_2/simple_arithmetic.py
+++ class_2/simple_arithmetic.py
@@ -7,7 +7,8 @@ result2 = x - y
 result3 = x * y
 result4 = x / y
 
-print("Exercise 1")
+print("""
+Exercise 1""")
 print(result1)
 print(result2)
 print(result3)
@@ -22,7 +23,8 @@ result6 = a - b
 result7 = a * b
 result8 = a / b
 
-print("Exercise 2")
+print("""
+Exercise 2""")
 print(result5)
 print(result6)
 print(result7)
@@ -39,7 +41,8 @@ div1 = d / f
 div2 = d // f
 mod = d % f
 
-print("Exercise 3")
+print("""
+Exercise 3""")
 print(add)
 print(sub)
 print(mul)
@@ -52,9 +55,12 @@ m = 2
 n = 4
 p = 5
 average = (m+n+p)/3
-print("Exercise 4 - Rounding")
+print("""
+Exercise 4 - Rounding""")
 print(round(average, 2))
 print(round((m+n+p)/3, 2))
 
 # determine if number is even or odd
-print(n % 2 == 0)
+print("""
+Exercise 5""")
+print(p % 2 == 0)
