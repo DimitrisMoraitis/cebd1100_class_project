cd5da87f2982108bf58d1573a6b2fbcacd40a001
DimitrisMoraitis <dimitris.moraitis75@gmail.com>
2022-07-05 18:44:11 -0400

    updated with input and conversion to both F and c

--- class_2/temperature_conversions.py
+++ class_2/temperature_conversions.py
@@ -1,9 +1,10 @@
-# convert Celsius to Farenheit
+# convert a value into Celsius and Farenheit
 
-temp = input("Enter temperature in Celsius > ")
+temp = input("Enter temperature to convert > ")
+temp = int(temp)
 
-temperature = int(temp)
-
-farenheit = (9/5)*temperature+32
+farenheit = round((9/5)*temp + 32, 1)
+celsius = round((temp - 32) / (9/5), 1)
 
 print("The temperature in Fareneheit is " + str(farenheit) + " degrees F")
+print("The temperature in Celsius is " + str(celsius) + " degrees C")
