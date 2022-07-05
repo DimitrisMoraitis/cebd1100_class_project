fec6229704b809c9090fdb33720b9ce73454ca3c
DimitrisMoraitis <dimitris.moraitis75@gmail.com>
2022-07-05 18:56:08 -0400

    used \n for new lines instead of triple quotes

--- class_2/simple_arithmetic.py
+++ class_2/simple_arithmetic.py
@@ -7,8 +7,7 @@ result2 = x - y
 result3 = x * y
 result4 = x / y
 
-print("""
-Exercise 1""")
+print("\nExercise 1")
 print(result1)
 print(result2)
 print(result3)
@@ -23,8 +22,7 @@ result6 = a - b
 result7 = a * b
 result8 = a / b
 
-print("""
-Exercise 2""")
+print("\nExercise 2")
 print(result5)
 print(result6)
 print(result7)
@@ -41,8 +39,7 @@ div1 = d / f
 div2 = d // f
 mod = d % f
 
-print("""
-Exercise 3""")
+print("\nExercise 3")
 print(add)
 print(sub)
 print(mul)
@@ -55,12 +52,10 @@ m = 2
 n = 4
 p = 5
 average = (m+n+p)/3
-print("""
-Exercise 4 - Rounding""")
+print("\nExercise 4 - Rounding")
 print(round(average, 2))
 print(round((m+n+p)/3, 2))
 
 # determine if number is even or odd
-print("""
-Exercise 5""")
+print("\nExercise 5")
 print(p % 2 == 0)
--- class_2/temperature_conversions.py
+++ class_2/temperature_conversions.py
@@ -6,5 +6,5 @@ temp = int(temp)
 farenheit = round((9/5) * temp + 32, 1)
 celsius = round((temp - 32) / (9/5), 1)
 
-print("The temperature in Fareneheit is " + str(farenheit) + " degrees F")
-print("The temperature in Celsius is " + str(celsius) + " degrees C")
+print("\nThe temperature in Fareneheit is " + str(farenheit) + " degrees F")
+print("\nThe temperature in Celsius is " + str(celsius) + " degrees C")
