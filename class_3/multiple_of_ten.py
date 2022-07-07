5efe2ec73c5d84b8b4c3ac5e4fa3d2e0dfd8736a
DimitrisMoraitis <dimitris.moraitis75@gmail.com>
2022-07-07 19:50:38 -0400

    second option for multiple of 10 including zero option

--- class_3/multiple_of_ten.py
+++ class_3/multiple_of_ten.py
@@ -1,8 +1,20 @@
-value = int(input("Pick an integer > "))
+# value = int(input("Pick an integer > "))
+#
+# calculation = value % 10
+#
+# if calculation == 0:
+#   print("Your number is a multiple of ten!")
+# else:
+#   print("Your number is not a multiple of ten!")
 
-calculation = value % 10
+value = int(input("Pick an integer, greater than zero, > "))
 
-if calculation == 0:
-  print("Your number is a multiple of ten!")
+if value > 0:
+  calculation = value % 10
+  if calculation == 0:
+    print("Your number is a multiple of ten!")
+  else:
+    print("Your number is not a multiple of ten!")
+
 else:
-  print("Your number is not a multiple of ten!")
+  print("Your integer was not greater than zero!")
