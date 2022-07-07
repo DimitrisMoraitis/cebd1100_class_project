d93d9dad08e8c639f47d5e0f857e1af3d8fdcd29
DimitrisMoraitis <dimitris.moraitis75@gmail.com>
2022-07-07 19:46:37 -0400

    second option for leaner patron dinner message

--- class_3/dinner_patrons.py
+++ class_3/dinner_patrons.py
@@ -1,6 +1,15 @@
+# patrons = int(input("How many people in your dinner group? > "))
+#
+# if patrons > 8:
+#     print("Please wait for the next available table")
+# else:
+#     print("Your dinner table is ready!")
+
 patrons = int(input("How many people in your dinner group? > "))
 
-if patrons > 8:
-    print("Please wait for the next available table")
-else:
-    print("Your dinner table is ready!")
+print('Your table is ', end="")
+
+if patrons >= 8:
+    print ('not ', end="")
+
+print('ready')
