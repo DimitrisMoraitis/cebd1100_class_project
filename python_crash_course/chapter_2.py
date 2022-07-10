24207e0d3613eabde085698f5c1f4c567bba6128
DimitrisMoraitis <dimitris.moraitis75@gmail.com>
2022-07-10 16:12:24 -0400

    start from python crash course book

---
+++ python_crash_course/chapter_2.py
@@ -0,0 +1,32 @@
+# # page 21 - format, methods and f-strings
+# first_name = 'ada'
+# last_name = 'lovelace'
+# full_name = f"{first_name} {last_name}"
+# #
+# # print(f"Hello my dear {full_name.title()}")
+# #
+# # message = f"I cannot be without you my one and only {first_name.capitalize()}"
+# # print(message)
+#
+# # page 22 - whitespaces and tabs
+# print("languages to learn:\n\tenglish\n\tfrench and\n\tgerman".title())
+#
+# favorite_word = " 'Hercules' "
+# favorite_word = favorite_word.strip()
+#
+# print(favorite_word)
+
+# # Exercises 2.3 to 2.7
+# famous_person = "albert eistein"
+#
+# message = f"{famous_person.title()} once said:\n\t'A person who never made a mistake\n\t never tied anything new'"
+#
+# print(message)
+#
+# print(2+3)
+
+# multiple assignment
+x, y, z = 1, 2, 3
+print(x, y, z)
+print (x+y)
+print(z-x)
